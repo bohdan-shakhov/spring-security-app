fc31d31a181187ff89c91b71ac1e04179ed675b9
Bohdan Shakhov <bogdanshahov2000@gmail.com>
2022-04-01 15:29:02 +0300

    add userDetails for authenticate need add passwordEncoder!

--- src/main/java/com/knubisoft/acolyte/springsecurityapp/security/ApplicationSecurityConfig.java
+++ src/main/java/com/knubisoft/acolyte/springsecurityapp/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.knubisoft.acolyte.springsecurityapp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
@@ -18,5 +23,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                .username("bohdan")
+                .password("marvel10")
+                .roles("STUDENT")
+                .build(); // ROLE_STUDENT
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
